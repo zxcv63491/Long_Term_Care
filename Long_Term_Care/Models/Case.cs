f0e5dc8498bbd2c64794d3c8216636f7bbd6fa74
zxcv63491 <154941268+zxcv63491@users.noreply.github.com>
2024-03-24 17:36:54 +0800

    Add files via upload

---
+++ Long_Term_Care/Models/Case.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Long_Term_Care.Models;
+
+public partial class Case
+{
+    public int CaseId { get; set; }
+
+    public string CaseName { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+
+    public DateOnly Birthdate { get; set; }
+
+    public int Age { get; set; }
+
+    public string IdentificationNumber { get; set; } = null!;
+
+    public string IdentityType { get; set; } = null!;
+
+    public string? CasePhone { get; set; }
+
+    public string City { get; set; } = null!;
+
+    public string District { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string FamilyName { get; set; } = null!;
+
+    public string FamilyPhone { get; set; } = null!;
+
+    public string Relation { get; set; } = null!;
+
+    public byte[]? CaseAvatar { get; set; }
+
+    public int? MemberId { get; set; }
+
+    public virtual ICollection<AppointmentForm> AppointmentForms { get; set; } = new List<AppointmentForm>();
+
+    public virtual ICollection<CareRecord> CareRecords { get; set; } = new List<CareRecord>();
+
+    public virtual Member? Member { get; set; }
+
+    public virtual ICollection<Member> Members { get; set; } = new List<Member>();
+}
